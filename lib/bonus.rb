cdaf13b4248a8bc23cc6e9fc09d2b2c501adcd62
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-01 18:25:58 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ def bonus
   }
 
 epic_tragedy[:montague][:hero][:status] = "dead"
-
-
+epic_tragedy[:montague][:patriarch][:status] = "dead"
+#Make changes in hash 
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
